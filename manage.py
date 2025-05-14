cc226c9d24cd090895b12f1403c265863b43b7b5
aya miida <22ad114@ms.dendai.ac.jp>
2025-05-14 15:52:21 +0900

    Render用にmigrate自動実行を追加

--- manage.py
+++ manage.py
@@ -15,6 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    os.system('python manage.py migrate')
+
     execute_from_command_line(sys.argv)
 
 
